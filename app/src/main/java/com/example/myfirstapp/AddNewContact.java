ae33e0450b76d8f9131cc77a36682aebf123b11c
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2020-06-25 22:59:12 +0200

    Create class AddNewCustomer

---
+++ app/src/main/java/com/example/myfirstapp/AddNewContact.java
@@ -0,0 +1,14 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_contact);
+    }
+}
